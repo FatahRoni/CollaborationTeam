54f249653fff99b598fc7c2faf620a99e146c35f
Rifqi Padi Siliwangi <79899198+Rifqips@users.noreply.github.com>
2022-12-20 20:19:32 +0700

    merge from dev roni

--- app/src/main/java/com/example/collaborationteam/Features/login/LoginActivity.kt
+++ app/src/main/java/com/example/collaborationteam/Features/login/LoginActivity.kt
@@ -1,25 +1,15 @@
 package com.example.collaborationteam.Features.login
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.collaborationteam.R
-<<<<<<< HEAD
-import com.example.collaborationteam.databinding.ActivityMainBinding
-import com.example.collaborationteam.databinding.ActivityRegisterBinding
-
-class LoginActivity : AppCompatActivity() {
-
-    private lateinit var binding : ActivityMainBinding
-
-=======
+import androidx.appcompat.app.AppCompatActivity
 import com.example.collaborationteam.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding : ActivityLoginBinding
->>>>>>> Dev-Roni
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }
