9e7f614f9ddc8286ae6965b907357f532c1d3aed
FatahRoni <fatahroni977@gmail.com>
2022-12-20 20:09:07 +0700

    Slicing login

--- app/src/main/java/com/example/collaborationteam/Features/login/LoginActivity.kt
+++ app/src/main/java/com/example/collaborationteam/Features/login/LoginActivity.kt
@@ -3,8 +3,10 @@ package com.example.collaborationteam.Features.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.collaborationteam.R
+import com.example.collaborationteam.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
